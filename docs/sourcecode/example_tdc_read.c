0fe61daa46a9cd2a5004591dda94b21f5b7db686
Matthias Schär <mati.schaer95@gmail.com>
2025-01-18 13:47:51 +0100

    [docs] added text for sub sub chapters "Fabrikation" and "Firmware"

---
+++ docs/sourcecode/example_tdc_read.c
@@ -0,0 +1,11 @@
+uint64_t tof_fs = 0;
+/* setup measurement */
+TDC_start(&tdc_opt);
+
+/* ... generate start and stop signals */
+
+if (TDC_read_result(&tdc_opt, &tof_fs) != TDC_OK) {
+	for (;;) {} // Error handling...
+}
+
+/* tof_fs now holds the measurement results, in fs */
